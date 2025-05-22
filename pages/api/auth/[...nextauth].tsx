79d1aac1a89df4def59f333ca1189843d369646a
Mohammed Aydi <maydi@itgsoftware.com>
2025-05-22 16:10:42 +0300

    zitadel auth

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -92,6 +92,6 @@ const authOptions : AuthOptions = {
 };
 
 
-const handler =  NextAuth(authOptions);
+export default NextAuth(authOptions);
 
-export {handler as GET, handler as POST}
+// export {handler as GET, handler as POST}
