6311024c9379e81f18425c49e9636f8a67f146b9
Mohammed Aydi <maydi@itgsoftware.com>
2025-05-29 00:05:38 +0300

    dep issues

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -86,6 +86,15 @@ export const authOptions : AuthOptions = {
           secure: true,
         },
       },
+      state: {
+        name: `__Secure-next-auth.state`,
+        options: {
+          httpOnly: true,
+          sameSite: "lax",
+          path: "/",
+          secure: true,
+        },
+      },
     },
     debug: true,  // ← right here!
   callbacks: {
