f96f365346b297a1a0c3384519d7435edb8ae3eb
Mohammed Aydi <maydi@itgsoftware.com>
2025-05-29 12:04:55 +0300

    pages updates

--- pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import SessionContextWrapper from "@/context/session-context-wrapper";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <SessionContextWrapper><Component {...pageProps} /></SessionContextWrapper> 
-}
